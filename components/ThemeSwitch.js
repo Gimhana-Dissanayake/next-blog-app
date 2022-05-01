10b0cfd5a959e90fe838fb16514c26397186e55a
GimhanaDissanayake <gimhanadev@gmail.com>
2022-05-01 10:29:37 +0530

    29. dark mode styles

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -19,6 +19,7 @@ function ThemeSwitch() {
           button {
             background: none;
             border: none;
+            color: inherit;
             cursor: pointer;
           }
         `}
